b9eea61a3f57110b9a2f7e0117a54b0fc15e2ae1
Marie-Claire-225 <marieclaiurenda@gmail.com>
2023-07-07 22:18:54 +0000

    second commit de bloomtech

--- js/connexion.js
+++ js/connexion.js
@@ -6,3 +6,22 @@ function myFunction() {
       x.style.display = "none";
     }
   }
+
+  // Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+var moda = document.getElementById('id02');
+
+// When the user clicks anywhere outside of the moda, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
